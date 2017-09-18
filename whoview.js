5a1ff0125447647c8f9676e4ee4b82a0a6124259
kyleplo <31634240+kyleplo@users.noreply.github.com>
2017-09-18 17:30:31 -0400

    Add postComment function

--- whoview.js
+++ whoview.js
@@ -1,7 +1,8 @@
 function whoviewlog(log,data){console.log("WhoView: "+log);if(data){console.log({type:"Whoview log data",data:data})}};
+function postComment(comment){document.forms["main-post-form"].elements.content.innerHTML = comment;document.forms["main-post-form"].getElementsByTagName("a")[0].click()}
 whoviewlog("Ready!");
 document.forms["main-post-form"].elements.content.setAttribute("placeholder","Leave a comment to appear in WhoView");
-chrome.storage.local.get({private:false},function (items){if(items.private){document.getElementById("comment-form").style.display = "none";
+chrome.storage.local.get({private:false,comment:""},function (items){if(items.comment.length > 0 && !items.private){setTimeout(function (){postComment(items.comment + " Visit https://scratch.mit.edu/projects/174950057/ to install WhoView.")},5000)};if(items.private){document.getElementById("comment-form").style.display = "none";
 document.getElementById("add-to").style.display = "none";whoviewlog("Private mode: Stuff removed")}});
 var projectViews = document.getElementsByClassName("views")[0].innerHTML;
 var commenters = document.getElementsByClassName("name");
@@ -17,4 +18,5 @@ if(commentUsers.indexOf(owner) === -1){commentUsers.push(owner)};
 if(commentUsers.indexOf(you) === -1){commentUsers.push(you)};
 whoviewlog("Sending data:",{"type":"project","views":projectViews,"commenters":commentUsers});
 chrome.runtime.sendMessage({"type":"project","views":projectViews,"commenters":commentUsers});
-whoviewlog("Data sent!!")
+whoviewlog("Data sent!!");
+document.getElementById("comments-enabled-box").innerHTML += " to not recieve WhoView notifications for this project.";
